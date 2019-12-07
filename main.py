7b829792ee9ff85813524abe0632d6064387ab97
Kataiser <mecharon1.gm@gmail.com>
2019-12-06 22:46:44 -0600

    Cleanup and clarification

--- main.py
+++ main.py
@@ -32,10 +32,9 @@ def main():
 
     # build a list of line numbers that are valid inputs
     valid_line_nums = []
-    for possible_line in enumerate(celeste_tas):
-        if input_file_trims[0] < possible_line[0] < (len(celeste_tas) + input_file_trims[1]):
-            if ',' in possible_line[1] and '#' not in possible_line[1] and 'Read' not in possible_line[1]:
-                valid_line_nums.append(possible_line[0])
+    for possible_line in enumerate(celeste_tas[input_file_trims[0]:input_file_trims[1]]):
+        if ',' in possible_line[1] and '#' not in possible_line[1] and 'Read' not in possible_line[1]:
+            valid_line_nums.append(possible_line[0])
 
     if settings()['random_order']:
         random.shuffle(valid_line_nums)
@@ -139,7 +138,7 @@ def parse_save_file(save_path: str) -> dict:
         elif '<TotalStrawberries>' in line:
             soup = BeautifulSoup(line, 'lxml')  # definitely overkill
             totalstrawberries = soup.find('totalstrawberries')
-            parsed['berries'] = int(totalstrawberries.text)
+            parsed['total_berries'] = int(totalstrawberries.text)
 
     return parsed
 
