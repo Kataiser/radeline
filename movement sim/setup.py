8bb4f8219a7a091a3dc92fabaadac0af91288b71
Kataiser <mecharon1.gm@gmail.com>
2025-05-02 12:09:22 -0500

    Fixed CD

--- movement sim/setup.py
+++ movement sim/setup.py
@@ -8,7 +8,7 @@ def main():
     os.chdir('movement sim')
     os.rename('sim.py', 'sim_compiled.py')
     setup(name='sim_compiled', ext_modules=cythonize('sim_compiled.py', nthreads=2, annotate=True,
-                                                     compiler_directives={'warn.unused': True, 'warn.unused_arg': True, 'warn.unused_result': True}))
+                                                     compiler_directives={'language_level': '3str', 'warn.unused': True, 'warn.unused_arg': True, 'warn.unused_result': True}))
     os.rename('sim_compiled.py', 'sim.py')
 
 
